b5b2df78fae071f6b4e7b0d944e873ca4684ce21
Piinxluna <pingpingkuss@gmail.com>
2022-11-26 22:15:39 +0700

    เปลี่ยนตัวอักษรตามช่องอื่น

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Hello world");
+console.log("ฮั่นแน่~ มีคนแอบดู");
+
+function changeData(index, data) {
+  var inputBox = document.getElementsByClassName("index-" + index);
+  for (var i = 0; i < inputBox.length; i++) {
+    inputBox[i].value = data;
+  }
+  //   console.log("index-" + index, data);
+}
