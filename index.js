fe76b6e1b0eca007461c9427ffc867d3f3037cc4
Piinxluna <pingpingkuss@gmail.com>
2022-11-27 13:40:21 +0700

    ทำให้คำตอบเชื่อมกัน + copy คำตอบ

--- index.js
+++ index.js
@@ -6,5 +6,39 @@ function changeData(index, data) {
   for (var i = 0; i < inputBox.length; i++) {
     inputBox[i].value = data;
   }
+  disableCopyAnsButton(false);
   //   console.log("index-" + index, data);
+}
+
+function copyAnswer() {
+  var space = [7, 11, 19, 25];
+  var ans = "";
+  for (var i = 1; i <= 30; i++) {
+    if (space.includes(i)) {
+      ans += " ";
+    } else {
+      thisValue = document.getElementById("ans-" + i).value;
+      // console.log(thisValue);
+      if (thisValue === "") {
+        ans += "?";
+      } else {
+        ans += thisValue;
+      }
+    }
+  }
+  ans = ans.toUpperCase();
+  // console.log(ans);
+  navigator.clipboard.writeText(ans);
+  disableCopyAnsButton(true);
+}
+
+function disableCopyAnsButton(isDisable) {
+  const button = document.getElementById("copy-ans-btn");
+  if (isDisable == true) {
+    button.innerText = "คัดลอกคำตอบแล้ว";
+    button.disabled = true;
+  } else {
+    button.innerText = "คัดลอกคำตอบ";
+    button.disabled = false;
+  }
 }
